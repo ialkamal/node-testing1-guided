00e4ac4ae14151835a412ab9c62a405bf282a4ff
Ismail Al Kamal <ismail@nawatt.com>
2021-02-16 07:16:33 +0300

    tests created

--- car/car.js
+++ car/car.js
@@ -1,1 +1,16 @@
 // Build a Car class!
+class Car {
+  constructor(args) {
+    this.model = args.model;
+    this.date = args.date;
+    this.odometer = 0;
+  }
+
+  drive(...args) {
+    const total = args.reduce((sum, distance) => (sum += distance), 0);
+    this.odometer += total;
+    return total;
+  }
+}
+
+module.exports = Car;
--- car/car.spec.js
+++ car/car.spec.js
@@ -1,1 +1,30 @@
 // Test away!
+const Car = require("./car");
+
+describe("Testing my Car class", () => {
+  let myCar;
+  let prius;
+
+  beforeEach(() => {
+    myCar = new Car({ model: "honda", date: "2015" });
+    prius = new Car({ model: "toyota", date: "2015" });
+  });
+
+  it("initial test", () => {
+    expect(myCar).toBeDefined();
+    expect(myCar).toEqual({ model: "honda", date: "2015", odometer: 0 });
+  });
+
+  test.todo("car should have a property");
+
+  it("has a drive method", () => {
+    expect(myCar.drive).toBeDefined();
+    expect(myCar.drive).toBeTruthy();
+    expect(myCar.drive).toBeInstanceOf(Function);
+    expect(myCar.drive).toBe(Car.prototype.drive);
+  });
+
+  test("drive supports comma-separated leg distances", () => {
+    expect(prius.drive(5, 3, 4)).toBe(12);
+  });
+});
